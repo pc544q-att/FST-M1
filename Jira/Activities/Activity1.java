af138ff6413756ce68fed80c1f4661f63b621c87
pc544q-att <151618730+pc544q-att@users.noreply.github.com>
2024-06-17 11:05:09 +0530

    Add files via upload

---
+++ Jira/Activities/Activity1.java
@@ -0,0 +1,17 @@
+package activities;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Car car = new Car(4, 4);	
+		
+		car.displaycharacteristics("Black", "Manual", 2014);
+		car.accelarate();
+		car.brake();
+		
+
+	}
+
+}
